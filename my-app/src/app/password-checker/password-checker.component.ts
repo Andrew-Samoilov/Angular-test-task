379773875b136b9d734c951c1f7464c9b49a6912
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-07-18 15:25:38 +0300

    refactor: setColor function

--- my-app/src/app/password-checker/password-checker.component.ts
+++ my-app/src/app/password-checker/password-checker.component.ts
@@ -19,48 +19,8 @@ export class PasswordChecker {
   constructor(private passwordCheckerService: PasswordCheckerService) { }
 
   setColor(n: number): string {
-    let color1 = '';
-    let color2 = '';
-    let color3 = '';
-
-    if (this.applyForm.value.password?.length === 0) {
-      color1 = 'LightGray';
-      color2 = 'LightGray';
-      color3 = 'LightGray';
-    }
-
-    const PASSWORD_STRENGTH = this.passwordCheckerService.checkPasswordStrength(this.applyForm.value.password ?? '');
-
-    switch (PASSWORD_STRENGTH) {
-      case `less8`:
-        color1 = 'red';
-        color2 = 'red';
-        color3 = 'red';
-        break;
-      case `easy`:
-        color1 = 'red';
-        break;
-      case `medium`:
-        color1 = 'yellow';
-        color2 = 'yellow';
-        break;
-      case `strong`:
-        color1 = 'MediumSpringGreen';
-        color2 = 'MediumSpringGreen';
-        color3 = 'MediumSpringGreen';
-        break;
-    }
-
-    switch (n) {
-      case 1:
-        return color1;
-      case 2:
-        return color2;
-      case 3:
-        return color3;
-      default:
-        return 'gray';
-    }
-
+    const password = this.applyForm.value.password ?? '';
+    return this.passwordCheckerService.setColor(password, n);
   }
+  
 }
--- my-app/src/app/services/password-checker.service.ts
+++ my-app/src/app/services/password-checker.service.ts
@@ -28,4 +28,46 @@ export class PasswordCheckerService {
       }
     }
   }
+
+  setColor(password: string, n: number): string {
+    let color1 = '';
+    let color2 = '';
+    let color3 = '';
+
+    if (password.length === 0) {
+      color1 = 'LightGray';
+      color2 = 'LightGray';
+      color3 = 'LightGray';
+    }
+
+    switch (this.checkPasswordStrength(password)) {
+      case `less8`:
+        color1 = 'red';
+        color2 = 'red';
+        color3 = 'red';
+        break;
+      case `easy`:
+        color1 = 'red';
+        break;
+      case `medium`:
+        color1 = 'yellow';
+        color2 = 'yellow';
+        break;
+      case `strong`:
+        return 'MediumSpringGreen';
+    }
+
+    switch (n) {
+      case 1:
+        return color1;
+      case 2:
+        return color2;
+      case 3:
+        return color3;
+      default:
+        return 'gray';
+    }
+
+  }
+
 }
